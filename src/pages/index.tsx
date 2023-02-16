593ecea6d34052b82b72ca4ea944245731bae13d
julienbrg <julien@strat.cc>
2023-02-16 15:05:21 +0100

    fix signer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Head } from '../components/layout/Head'
 import Image from 'next/image'
 import { LinkComponent } from '../components/layout/LinkComponent'
 import { useState, useEffect } from 'react'
-import { useSigner, useAccount, useBalance, useNetwork } from 'wagmi'
+import { useSigner, useAccount, useBalance, useNetwork, useProvider } from 'wagmi'
 import { ethers } from 'ethers'
 import { NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, METADATA_NFT_1, METADATA_NFT_2, METADATA_NFT_3 } from '../utils/config'
 
@@ -14,7 +14,7 @@ export default function Home() {
   const [isContractOwner, setIsContractOwner] = useState<boolean>(false)
 
   const { address } = useAccount()
-  // const provider = useProvider()
+  const provider = useProvider()
   const { data: signer } = useSigner()
   const { data: bal } = useBalance({
     address: address,
@@ -24,8 +24,6 @@ export default function Home() {
 
   const explorerUrl = network.chain?.blockExplorers?.default.url
 
-  const nft = new ethers.Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, signer as any)
-
   useEffect(() => {
     const val = Number(bal?.formatted).toFixed(3)
     setUserBal(String(val) + ' ' + bal?.symbol)
@@ -34,7 +32,10 @@ export default function Home() {
 
   const checkOwnership = async () => {
     try {
+      const nft = new ethers.Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, provider as any)
+      console.log('address:', address)
       const call = await nft.owner()
+      console.log('owner:', call)
       if (call === address) {
         setIsContractOwner(true)
       } else {
@@ -48,6 +49,7 @@ export default function Home() {
   const mint = async () => {
     console.log('minting...')
     try {
+      const nft = new ethers.Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, signer as any)
       setLoading(true)
       const call = await nft.mint(METADATA_NFT_1, METADATA_NFT_2, METADATA_NFT_3)
       const nftReceipt = await call.wait(1)
@@ -63,7 +65,7 @@ export default function Home() {
       })
     } catch (e: any) {
       setLoading(false)
-      console.log('error:', e.code)
+      console.log('error:', e.message)
       if (e.code === 'UNPREDICTABLE_GAS_LIMIT') {
         toast({
           title: "CAN'T MINT TWICE",
@@ -76,7 +78,7 @@ export default function Home() {
       if (e.code === 'UNSUPPORTED_OPERATION') {
         toast({
           title: "CAN'T MINT",
-          description: "You are not the owner of this NFT contract, therefore you can't call the mint function.",
+          description: 'You are not allowed to call the mint function.',
           status: 'error',
           duration: 9000,
           isClosable: true,
@@ -96,11 +98,17 @@ export default function Home() {
         <br />
         <p>You&apos;re about to mint 3 NFTs on Ethereum Goerli Testnet. We&apos;ll switch to Ethereum Mainnet when the time&apos;s right.</p>
         <br />
-        <p>
-          You&apos;re connected to <strong>{network.chain?.name ?? 'Ethereum'}</strong> and your wallet currently holds
-          <strong> {userBal}</strong>. Just click on the &apos;Mint&apos; button below: you will be invited to sign your transaction: you will mint 3
-          different NFTs.{' '}
-        </p>
+        {!address ? (
+          <p style={{ color: 'red' }}>
+            <strong>Please connect your wallet</strong>.{' '}
+          </p>
+        ) : (
+          <p>
+            You&apos;re connected to <strong>{network.chain?.name ?? 'Ethereum'}</strong> and your wallet currently holds
+            <strong> {userBal}</strong>. Just click on the &apos;Mint&apos; button below: you will be invited to sign your transaction: you will mint
+            3 different NFTs.{' '}
+          </p>
+        )}
         <br />
         {!isContractOwner ? (
           <p style={{ color: 'red' }}>
