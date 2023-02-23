c4c5dc7840065a22e8609d5f12aec3bc4f44e2b2
julienbrg <julien@strat.cc>
2023-02-23 15:58:51 +0100

    add test

--- hardhat/test/NFT.ts
+++ hardhat/test/NFT.ts
@@ -2,21 +2,10 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
 
-/*
-  Ato standard contract : ERC721 with resale rights
-*/
 describe('=== NFT ===', function () {
   async function deployContractsFixture() {
-    // Create signers
     const [issuer, acquirer] = await ethers.getSigners()
 
-    // const name = 'Black Thistle'
-    // const symbol = 'THISTLE'
-    // const uri = 'bafybeihjf5qkgkxepvs4mmmd77hsw2yw52py6j2blcl4tzjxgrn6aixs7a/metadata.json'
-    // const mintNumber = 1
-    // const royalties = 1.5 * 100 // 1.5% resale rights
-
-    // Create instance of Ato.sol
     const NFT = await ethers.getContractFactory('NFT')
     const nft = await NFT.deploy()
     await nft.deployed()
